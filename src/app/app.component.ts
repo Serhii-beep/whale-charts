b88dcb9535885df6a98586df619cd8c8f0229ea0
Serhii Peshko <peskosergij@gmail.com>
2024-02-15 12:56:50 -0500

    add new info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component, ViewChild, OnDestroy } from '@angular/core';
+import { Component, ViewChild, OnDestroy, OnInit } from '@angular/core';
 import { Chart, ChartConfiguration, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import Annotation from 'chartjs-plugin-annotation';
-import { EMPTY, Subject, Subscription, catchError, of, takeUntil, tap } from 'rxjs';
+import { EMPTY, Subject, Subscription, catchError, forkJoin, of, switchMap, takeUntil, tap } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
@@ -10,7 +10,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent implements OnInit, OnDestroy {
   private dataset: number[] = [65, 59, 80, 81, 56, 55, 40];
   public apiKey: string = '';
   public apiKeyValid: boolean = false;
@@ -20,6 +20,11 @@ export class AppComponent implements OnDestroy {
 
   constructor(private httpClient: HttpClient) {
     Chart.register(Annotation);
+  }
+
+  ngOnInit(): void {
+    this.apiKey = localStorage.getItem('label_studio_api_key') ?? '';
+    this.submitKey();
   }
 
   ngOnDestroy(): void {
@@ -122,24 +127,48 @@ export class AppComponent implements OnDestroy {
   public submitKey(): void {
     if(this.apiKey) {
       this.apiKeyValid = true;
+      localStorage.setItem('label_studio_api_key', this.apiKey);
       if(this.interval) {
         clearInterval(this.interval);
       }
+      this.getData();
       this.interval = setInterval(() => {
-        if(this.currentSubscription) {
-          this.currentSubscription.unsubscribe();
+        console.log('Sending new request...');
+        this.getData();
+      }, 15000);
+    }
+  }
+
+  private getData(): void {
+    if(this.currentSubscription) {
+      this.currentSubscription.unsubscribe();
+    }
+    const headers = new HttpHeaders().set('Authorization', `Token ${this.apiKey}`);
+    this.currentSubscription = this.httpClient.get('http://129.97.251.100:8080/api/tasks?project=52', { headers }).pipe(
+      takeUntil(this.unsubscribe$),
+      switchMap((res: any) => {
+        if(!res || res.length === 0) {
+          return of(EMPTY);
         }
-        const headers = new HttpHeaders().set('Authorization', `Token ${this.apiKey}`);
-        this.currentSubscription = this.httpClient.get('http://129.97.251.100:8080/api/tasks?page=1&page_size=-1&project=33', { headers }).pipe(
-          takeUntil(this.unsubscribe$),
-          tap(res => console.log(res)),
+        const tasks = res.tasks;
+        console.log('Tasks', tasks);
+        const tasksRequests = tasks.map((task: any) => this.httpClient.get(`http://129.97.251.100:8080/api/tasks/${task.id}/annotations`, { headers }));
+
+        return forkJoin(tasksRequests).pipe(
           catchError(error => {
-            this.apiKeyValid = false;
             return of(EMPTY);
           })
-        ).subscribe();
-      }, 5000);
-    }
+        );
+      }),
+      tap(resp => {
+        console.log('Annotations for each task', resp);
+      }),
+      catchError(error => {
+        console.log(error);
+        this.apiKeyValid = false;
+        return of(EMPTY);
+      })
+    ).subscribe();
   }
 
   private calculateMean(numbers: number[]): number {
